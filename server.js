2e819107aacffa15433617b62fc401f9dfaa6168
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-02-24 17:54:02 +0100

    GET

--- server.js
+++ server.js
@@ -5,3 +5,12 @@ const PORT = 8000;
 const app = express();
 
 app.listen(PORT, () => console.log(`Server working on PORT ${PORT}`));
+
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham",
+};
+
+app.get("/", (req, res) => {
+  res.json(celebrity);
+});
