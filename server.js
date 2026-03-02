cbe56d1d5a1eda8561296bc6f91ed027dfcd07c2
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-02 11:32:31 +0100

    Route Not Found

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use("/api", apiRouter);
 
+//Route not found
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Endpoint not found. Please check the API documentation.",
+  });
+});
+
 app.listen(PORT, () =>
   console.log(`Server working on http://localhost:${PORT}`),
 );
