b699588daaa3a609af31b909b6dde5715af68fd0
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-02 16:18:36 +0100

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [startups, setStartups] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api")
+      .then((res) => res.json())
+      .then((data) => setStartups(data))
+      .catch((err) => console.error("Error connecting to API:", err));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App-div">
+      <h1>Startup Atlas</h1>
+      <div className="card-container">
+        {startups.map((startup, index) => (
+          <div key={index} className="startup-card">
+            <h2>{startup.name}</h2>
+            <p>
+              <strong>Industry:</strong> {startup.industry}
+            </p>
+            <p>
+              <strong>Location:</strong> {startup.country}, {startup.continent}
+            </p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
