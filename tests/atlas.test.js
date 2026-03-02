f47f6e5819b549f763f81c5192246c4a872deb53
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-02 15:46:04 +0100

    test query parameters

--- tests/atlas.test.js
+++ tests/atlas.test.js
@@ -5,6 +5,8 @@ import { apiRouter } from "../routes/apiRoutes.js";
 
 //Mock app to test the routes
 
+//getDataByParams - PATH PARAMETERS
+
 const app = express();
 app.use("/api", apiRouter);
 
@@ -16,7 +18,9 @@ describe("Startup Atlas - Path Parameters", () => {
     expect(res.statusCode).toBe(200);
     expect(Array.isArray(res.body)).toBe(true);
 
-    expect(res.body.some((s) => s.name === "TechNova AI")).toBe(true);
+    expect(res.body.some((startup) => startup.name === "TechNova AI")).toBe(
+      true,
+    );
   });
 
   test("Failure:should block invalid fields with 400 error", async () => {
@@ -35,4 +39,34 @@ test("Success:should return an empty array if the term is not found", async () =
   expect(res.statusCode).toBe(200); //Because the req is valid
   expect(Array.isArray(res.body)).toBe(true); //It should be an array
   expect(res.body.length).toBe(0); //but empty
+});
+
+//getAllData QUERY PARAMETERS
+
+describe("Startup Atlas - Query Parameters", () => {
+  test("Success:should filter by multiple queries(industry and has_mvp", async () => {
+    const res = await request(app)
+      .get("/api")
+      .query({ industry: "AI", has_mvp: "true" });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+
+    res.body.forEach((startup) => {
+      expect(startup.industry.toLowerCase()).toBe("ai");
+      expect(startup.has_mvp).toBe(true);
+    });
+  });
+
+  test("Success:should filter boolean fields like is_seeking_funding", async () => {
+    const res = await request(app)
+      .get("/api")
+      .query({ is_seeking_funding: "false" });
+
+    expect(res.statusCode).toBe(200);
+
+    expect(
+      res.body.every((startup) => startup.is_seeking_funding === false),
+    ).toBe(true);
+  });
 });
