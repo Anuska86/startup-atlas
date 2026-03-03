04ac09a4f65bbd0902f93987259884c4ea594052
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-03 16:51:39 +0100

    Update MapPage.jsx

--- client/src/components/MapPage.jsx
+++ client/src/components/MapPage.jsx
@@ -1,5 +1,4 @@
 import "../styles/MapPage.css";
-import QMLogo from "../assets/qm.png";
 
 import { useEffect, useRef } from "react";
 
@@ -25,17 +24,15 @@ L.Marker.prototype.options.icon = DefaultIcon;
 
 function MapPage({ startups, theme }) {
   const center = [20, 0]; // center of the map
-  const zoom = 15;
   const mapRef = useRef(null);
+  const zoom = 18;
 
   //recheck the size of the map and redraw the tiles
   useEffect(() => {
-    if (mapRef.current) {
-      setTimeout(() => {
-        mapRef.current.invalidateSize();
-      }, 100);
-    }
-  }, [theme, startups]);
+    setTimeout(() => {
+      mapRef.current.invalidateSize();
+    }, 100);
+  }, [theme]);
 
   //Business logos
 
@@ -66,8 +63,8 @@ function MapPage({ startups, theme }) {
       html: iconMarkup,
       className: "industry-icon-marker",
       iconSize: [30, 30],
-      iconAnchor: [16, 16],
-      popupAnchor: [0, -16],
+      iconAnchor: [15, 15],
+      popupAnchor: [0, -15],
     });
   };
 
@@ -80,48 +77,51 @@ function MapPage({ startups, theme }) {
         ref={mapRef}
         style={{ height: "70vh", width: "100%" }}
       >
-        {/* LIGHT TILES LAYER */}
+        {/* THEME TILES LAYER */}
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          key={theme}
+          url={
+            theme === "light"
+              ? "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+              : "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          }
           attribution="&copy; CARTO"
-          opacity={theme === "light" ? 1 : 0}
-          className="map-tile-layer"
-          zIndex={theme === "light" ? 10 : 1} // Bring to front when active
         />
-        {/* DARK TILES LAYER */}
-        <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-          attribution="&copy; CARTO"
-          opacity={theme === "dark" ? 1 : 0}
-          className="map-tile-layer"
-          zIndex={theme === "dark" ? 10 : 1} // Bring to front when active
-        />
+
         <MarkerClusterGroup
           chunkedLoading // Optimization for many markers
           spiderfyOnMaxZoom={true}
         >
-          {startups.map((startup) => (
-            <Marker
-              key={startup.id}
-              position={[startup.lat, startup.lng]}
-              icon={createCustomIcon(startup)}
-            >
-              <Popup>
-                <div className="map-popup">
-                  <h3>{startup.name}</h3>
-                  <p>
-                    <strong>{startup.industry}</strong>
-                  </p>
-                  <p>
-                    {startup.business_address.city}, {startup.country}
-                  </p>
-                  <a href={startup.website} target="_blank" rel="noreferrer">
-                    Visit Website
-                  </a>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
+          {startups.map(
+            (startup) =>
+              startup.lat &&
+              startup.lng && (
+                <Marker
+                  key={startup.id}
+                  position={[startup.lat, startup.lng]}
+                  icon={createCustomIcon(startup)}
+                >
+                  <Popup>
+                    <div className="map-popup">
+                      <h3>{startup.name}</h3>
+                      <p>
+                        <strong>{startup.industry}</strong>
+                      </p>
+                      <p>
+                        {startup.business_address.city}, {startup.country}
+                      </p>
+                      <a
+                        href={startup.website}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Visit Website
+                      </a>
+                    </div>
+                  </Popup>
+                </Marker>
+              ),
+          )}
         </MarkerClusterGroup>
       </MapContainer>
     </div>
