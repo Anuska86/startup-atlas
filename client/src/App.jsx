d7f79801d70cb829d477047ac7650d8a746df098
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-03 10:34:56 +0100

    handle search

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,39 @@ import { useState, useEffect } from "react";
 function App() {
   const [startups, setStartups] = useState([]);
 
+  //Fetching the data
   useEffect(() => {
     fetch("http://localhost:8000/api")
       .then((res) => res.json())
       .then((data) => setStartups(data))
       .catch((err) => console.error("Error connecting to API:", err));
   }, []);
+
+  //handle the search
+
+  const handleSearch = async (e) => {
+    if (e) e.preventDefault();
+
+    //If search is empty, then get all the data
+    if (!searchTerm) {
+      const res = await fetch("http://localhost:8000/api");
+      const data = await res.json();
+      setStartups(data);
+      return;
+    }
+
+    //Flexible path
+
+    try {
+      const res = await fetch(
+        `http://localhost:8000/api/industry/${searchTerm}`,
+      );
+      const data = await res.json();
+      setStartups(Array.isArray(data) ? data : []);
+    } catch (error) {
+      console.error("Search error:", error);
+    }
+  };
 
   return (
     <div className="App-div">
