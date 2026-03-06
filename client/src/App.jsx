21be65cd282a96d2fa2a32474dbb2a924092edae
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-03-06 15:57:40 +0100

    reset button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import {
   BiRocket,
   BiMoon,
   BiSun,
+  BiX,
 } from "react-icons/bi";
 
 function App() {
@@ -200,7 +201,7 @@ function App() {
               </button>
               {searchTerm && (
                 <button className="reset-btn" onClick={handleReset}>
-                  X Clear
+                  <BiX size={20} /> Clear
                 </button>
               )}
             </div>
