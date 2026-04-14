c600a66dbdcf125dc052907bab1e7b5139f78f86
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-04-14 10:41:05 +0200

    Update themeTransition.js

--- client/src/utils/themeTransition.js
+++ client/src/utils/themeTransition.js
@@ -8,29 +8,29 @@ export const runThemeTransition = (event, switchTheme) => {
     Math.max(x, innerWidth - x),
     Math.max(y, innerHeight - y),
   );
+
+  if (!document.startViewTransition) {
+    switchTheme();
+    return;
+  }
+
+  const transition = document.startViewTransition(() => {
+    switchTheme();
+  });
+
+  transition.ready.then(() => {
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0px at ${x}px ${y}px)`,
+          `circle(${endRadius}px at ${x}px ${y}px)`,
+        ],
+      },
+      {
+        duration: 500,
+        easing: "ease-in",
+        pseudoElement: "::view-transition-new(root)",
+      },
+    );
+  });
 };
-
-if (!document.startViewTransition) {
-  switchTheme();
-  return;
-}
-
-const transition = document.startViewTransition(() => {
-  switchTheme();
-});
-
-transition.ready.then(() => {
-  document.documentElement.animate(
-    {
-      clipPath: [
-        `circle(0px at ${x}px ${y}px)`,
-        `circle(${endRadius}px at ${x}px ${y}px)`,
-      ],
-    },
-    {
-      duration: 500,
-      easing: "ease-in",
-      pseudoElement: "::view-transition-new(root)",
-    },
-  );
-});
