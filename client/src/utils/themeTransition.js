d89a84ad9f7e5523ca22144d4718283518fc265e
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-04-14 10:34:04 +0200

    Create themeTransition.js

---
+++ client/src/utils/themeTransition.js
@@ -0,0 +1,34 @@
+export const runThemeTransition = (event, switchTheme) => {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  );
+};
+
+if (!document.startViewTransition) {
+  switchTheme();
+  return;
+}
+
+const transition = document.startViewTransition(() => {
+  switchTheme();
+});
+
+transition.ready.then(() => {
+  document.documentElement.animate(
+    {
+      clipPath: [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ],
+    },
+    {
+      duration: 500,
+      easing: "ease-in",
+      pseudoElement: "::view-transition-new(root)",
+    },
+  );
+});
