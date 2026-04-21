2a11a8ab5eabb157a90a104c81cd173d1432e232
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-04-21 12:19:33 +0200

    Update iconUtils.jsx

--- client/src/utils/iconUtils.jsx
+++ client/src/utils/iconUtils.jsx
@@ -1,18 +1,68 @@
 import L from "leaflet";
 import { renderToStaticMarkup } from "react-dom/server";
-import { BiChip, BiLeaf, BiLineChart, BiHeart } from "react-icons/bi";
+import {
+  BiChip,
+  BiLeaf,
+  BiLineChart,
+  BiHeart,
+  BiStore,
+  BiCodeAlt,
+  BiMusic,
+  BiHeadset,
+  BiCartAlt,
+  BiGlobe,
+} from "react-icons/bi";
 import { getCategoryColor } from "./helpers.js";
 
 export const createCustomIcon = (startup) => {
-  let SelectedIcon = BiChip;
+  //Prepare the data
+
   const industry = startup.industry?.toLowerCase() || "";
 
-  if (industry.includes("energy") || industry.includes("agriculture")) {
+  const tags = Array.isArray(startup.tags)
+    ? startup.tags.join("").toLowerCase()
+    : startup.tags?.toLowerCase() || "";
+
+  let SelectedIcon = BiChip; //Default icon
+
+  //Specific tags first
+  if (tags.includes("developer") || tags.includes("code")) {
+    SelectedIcon = BiCodeAlt;
+  } else if (tags.includes("music")) {
+    SelectedIcon = BiMusic;
+  } else if (
+    tags.includes("augmented") ||
+    tags.includes("vr") ||
+    tags.includes("game")
+  ) {
+    SelectedIcon = BiHeadset;
+  } else if (
+    tags.includes("grocery") ||
+    tags.includes("ecommerce") ||
+    tags.includes("shop")
+  ) {
+    SelectedIcon = BiCartAlt;
+  }
+
+  //Industry Fallback
+  else if (
+    industry.includes("energy") ||
+    industry.includes("agri") ||
+    industry.includes("environment")
+  ) {
     SelectedIcon = BiLeaf;
-  } else if (industry.includes("fin") || industry.includes("saas")) {
+  } else if (
+    industry.includes("fin") ||
+    industry.includes("saas") ||
+    industry.includes("b2b")
+  ) {
     SelectedIcon = BiLineChart;
-  } else if (industry.includes("health")) {
+  } else if (industry.includes("health") || industry.includes("bio")) {
     SelectedIcon = BiHeart;
+  } else if (industry.includes("consumer")) {
+    SelectedIcon = BiStore;
+  } else if (industry.includes("enterprise")) {
+    SelectedIcon = BiGlobe;
   }
 
   const iconMarkup = renderToStaticMarkup(
