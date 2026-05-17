a0a1135d8ed254612b67fcfc7a4e655944fc42b7
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2026-05-17 17:27:28 +0200

    Update ThemeContext.jsx

--- client/src/contexts/ThemeContext.jsx
+++ client/src/contexts/ThemeContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState, useEffect, useContext } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
